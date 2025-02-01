7e660953e80027c1203ee175a83a959d93636145
machbry <matthieu.chambery@gmail.com>
2025-02-01 14:11:20 +0100

    Revert "use constant to point toward data folder"
    This reverts commit acbfccc8729d6aa70b6f84f8cee9cb67a46a253e.

--- algorithme/potentiel_solaire/constants.py
+++ algorithme/potentiel_solaire/constants.py
@@ -1,5 +1,5 @@
 from pathlib import Path
 
 
-ALGORITHME_FOLDER = Path(__file__).parent.parent
-DATA_FOLDER = ALGORITHME_FOLDER / 'data'
+ROOT_FOLDER = Path(__file__).parent.parent.parent
+DATA_FOLDER = ROOT_FOLDER / 'data'
