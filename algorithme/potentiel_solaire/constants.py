b503f4227fd8e08667a320ff788a86e804837f10
K. Ghosh <kelu124@gmail.com>
2025-02-16 21:15:54 +0100

    Update constants.py w/ Lambert

--- algorithme/potentiel_solaire/constants.py
+++ algorithme/potentiel_solaire/constants.py
@@ -6,3 +6,4 @@ DATA_FOLDER = ALGORITHME_FOLDER / 'data'
 SCHOOLS_ESTABLISHMENTS_FILENAME = "fr-en-annuaire-education.geojson"
 CONTOURS_DEPARTEMENTS_FILENAME = "contour-des-departements.geojson"
 CRS = 4326
+CRS_LAMBERT = 2154
