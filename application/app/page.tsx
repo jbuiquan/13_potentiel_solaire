9962232f4a52f2483ab5d50a9b42cf5481b58318
Oceane BOUNAN <bounan.oceane@orange.fr>
2025-02-16 17:22:49 +0400

    Maplibre POC

--- application/app/page.tsx
+++ application/app/page.tsx
@@ -1,7 +1,10 @@
+import Map from "@/components/Map";
+
 export default function Home() {
   return (
     <main>
-      <div>Hello world!</div>
+      <div className="text-4xl font-extrabold dark:text-white">Carte de France</div>
+      <Map />
     </main>
   );
 }
--- application/tailwind.config.ts
+++ application/tailwind.config.ts
@@ -1,7 +1,10 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {},
   plugins: [],
 } satisfies Config;
