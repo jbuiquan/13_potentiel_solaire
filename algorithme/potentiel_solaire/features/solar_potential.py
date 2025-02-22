d58dc4d68aacb79c2d3e6ec12723f8d7c1075782
machbry <matthieu.chambery@gmail.com>
2025-02-22 16:20:46 +0100

    ajustements (pep8, idempotence, ...) sur le calcul du rayonnement solaire

--- algorithme/potentiel_solaire/features/solar_potential.py
+++ algorithme/potentiel_solaire/features/solar_potential.py
@@ -1,18 +1,32 @@
 import geopandas as gpd
 
-from potentiel_solaire.sources.bd_irr import getIrradiationEcole
+from potentiel_solaire.features.solar_exposition import calculate_solar_exposition_building
 from potentiel_solaire.features.roof_attributes import calculate_surface_utile
 from potentiel_solaire.constants import RENDEMENT_PANNEAU_PV
 
 
 def calculate_solar_potential(
     schools_buildings: gpd.GeoDataFrame,
+    bd_irradiation_path: str,
+    buffer_for_buildings_surroundings: int = 2000,
+    rendement_panneau_pv: float = RENDEMENT_PANNEAU_PV
 ) -> gpd.GeoDataFrame:
+    """Fonction principale pour calculer le potentiel solaire.
 
-    # Calcul de la surface totale au sol
+    :param schools_buildings: les batiments rataches a une ecole
+    :param bd_irradiation_path: chemin du fichier .tif des donnees d irradiation
+    :param buffer_for_buildings_surroundings: distance autour du batiment a prendre en compte
+    :param rendement_panneau_pv: ration du rendement d un panneau solaire
+    :return: le geodataframe des batiments scolaires avec les features de potentiel solaire
+    """
+
+    # Calcul de la surface totale au sol & zone de 2km autour des batiment
     crs_init = schools_buildings.crs
     schools_buildings = schools_buildings.to_crs(epsg=6933)  # pourquoi ? est-ce applicable aux DROMs ?
     schools_buildings["surface_totale_au_sol"] = schools_buildings.area
+    schools_buildings["zone_autour_du_batiment"] = schools_buildings.buffer(
+        distance=buffer_for_buildings_surroundings
+    ).to_crs(crs_init)
     schools_buildings = schools_buildings.to_crs(crs_init)
 
     # Calcul de la surface utile
@@ -22,22 +36,23 @@ def calculate_solar_potential(
        ), axis=1
     )
 
-    schools_buildings["protection"] = schools_buildings.apply(
-        lambda building: None, axis=1
-    )  # TODO : a implementer
+    # Calcul de l exposition solaire
+    schools_buildings["rayonnement_solaire"] = schools_buildings.apply(
+       lambda building: calculate_solar_exposition_building(
+           zone_surrounding_building=building["zone_autour_du_batiment"],
+           bd_irradiation_path=bd_irradiation_path
+       ), axis=1
+    )
 
-    schools_buildings = getIrradiationEcole(schools_buildings)
-    #schools_buildings["rayonnement_solaire"] = schools_buildings.apply(
-    #    lambda building: random.choice(range(0, 150)), axis=1
-    #)
-
+    # Calcul potentiel solaire
     schools_buildings["potentiel_solaire"] = \
         schools_buildings["rayonnement_solaire"] * \
         schools_buildings["surface_utile"] * \
-        RENDEMENT_PANNEAU_PV
+        rendement_panneau_pv
 
-    #schools_buildings["potentiel_solaire"] = schools_buildings.apply(
-    #    lambda building: random.choice(range(0, 1500)), axis=1
-    #)
+    # Ajout du tag batiments proteges ou en zone protegee
+    schools_buildings["protection"] = schools_buildings.apply(
+        lambda building: None, axis=1
+    )  # TODO : a implementer
 
     return schools_buildings
