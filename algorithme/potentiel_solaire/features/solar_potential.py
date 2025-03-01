f312c2dfd69dadc297524a5ecc9a4a8b43c3011d
machbry <matthieu.chambery@gmail.com>
2025-03-01 13:13:05 +0100

    homogenisation de l'utilisation des crs notamment pour le calcul de buffers en metres

--- algorithme/potentiel_solaire/features/solar_potential.py
+++ algorithme/potentiel_solaire/features/solar_potential.py
@@ -3,32 +3,38 @@ import geopandas as gpd
 from potentiel_solaire.features.solar_exposition import calculate_solar_exposition_building
 from potentiel_solaire.features.roof_attributes import calculate_surface_utile
 from potentiel_solaire.features.protected_tag import link_protected_buildings
-from potentiel_solaire.constants import RENDEMENT_PANNEAU_PV, BUFFER_SIZE_FOR_SOLAR_EXPOSITION
+from potentiel_solaire.constants import (
+    RENDEMENT_PANNEAU_PV,
+    BUFFER_SIZE_FOR_SOLAR_EXPOSITION,
+    CRS_FOR_BUFFERS
+)
 
 
 def calculate_solar_potential(
     schools_buildings: gpd.GeoDataFrame,
     bd_irradiation_path: str,
     areas_with_protected_buildings: gpd.GeoDataFrame,
-    buffer_for_buildings_surroundings: int = BUFFER_SIZE_FOR_SOLAR_EXPOSITION,
-    rendement_panneau_pv: float = RENDEMENT_PANNEAU_PV
+    buffer_for_solar_exposition: int = BUFFER_SIZE_FOR_SOLAR_EXPOSITION,
+    rendement_panneau_pv: float = RENDEMENT_PANNEAU_PV,
+    crs_for_buffers: int = CRS_FOR_BUFFERS
 ) -> gpd.GeoDataFrame:
     """Fonction principale pour calculer le potentiel solaire.
 
     :param schools_buildings: les batiments rataches a une ecole
     :param bd_irradiation_path: chemin du fichier .tif des donnees d irradiation
     :param areas_with_protected_buildings: gdf des zones avec des batiments proteges
-    :param buffer_for_buildings_surroundings: distance autour du batiment a prendre en compte
+    :param buffer_for_solar_exposition: distance autour du batiment a prendre en compte pour le rayonnement solaire
     :param rendement_panneau_pv: ration du rendement d un panneau solaire
+    :param crs_for_buffers: crs utilise pour le calcul des buffers (en metres)
     :return: le geodataframe des batiments scolaires avec les features de potentiel solaire
     """
 
     # Calcul de la surface totale au sol & zone de 2km autour des batiment
     crs_init = schools_buildings.crs
-    schools_buildings = schools_buildings.to_crs(epsg=6933)
+    schools_buildings = schools_buildings.to_crs(epsg=crs_for_buffers)
     schools_buildings["surface_totale_au_sol"] = schools_buildings.area
-    schools_buildings["zone_autour_du_batiment"] = schools_buildings.buffer(
-        distance=buffer_for_buildings_surroundings
+    schools_buildings["zone_pour_rayonnement_solaire"] = schools_buildings.buffer(
+        distance=buffer_for_solar_exposition
     ).to_crs(crs_init)
     schools_buildings = schools_buildings.to_crs(crs_init)
 
@@ -42,7 +48,7 @@ def calculate_solar_potential(
     # Calcul de l exposition solaire
     schools_buildings["rayonnement_solaire"] = schools_buildings.apply(
        lambda building: calculate_solar_exposition_building(
-           zone_surrounding_building=building["zone_autour_du_batiment"],
+           zone_for_solar_exposition=building["zone_pour_rayonnement_solaire"],
            bd_irradiation_path=bd_irradiation_path
        ), axis=1
     )
--- algorithme/potentiel_solaire/sources/protected_buildings.py
+++ algorithme/potentiel_solaire/sources/protected_buildings.py
@@ -1,12 +1,12 @@
 import geopandas as gpd
-from potentiel_solaire.constants import DATA_FOLDER, CRS, BUFFER_SIZE_FOR_PROTECTED_BUILDINGS
+from potentiel_solaire.constants import DATA_FOLDER, DEFAULT_CRS, BUFFER_SIZE_FOR_PROTECTED_BUILDINGS, CRS_FOR_BUFFERS
 from potentiel_solaire.logger import get_logger
 
 logger = get_logger()
 
 
 def extract_protected_buildings(
-    crs: int = CRS
+    crs: int = DEFAULT_CRS
 ) -> str:
     """Extrait les batiments proteges
 
@@ -36,23 +36,25 @@ def get_areas_with_protected_buildings(
     bd_protected_buildings_path: str,
     geom_of_interest: gpd.GeoDataFrame,
     buffer_size_for_protected_buildings: float = BUFFER_SIZE_FOR_PROTECTED_BUILDINGS,
-    crs: int = CRS
+    crs: int = DEFAULT_CRS,
+    crs_for_buffers: int = CRS_FOR_BUFFERS
 ):
     """Filtre et renvoit les batiments proteges sur la zone d'interet
 
     :param bd_protected_buildings_path: chemin du fichier .geojson des batiments proteges
     :param geom_of_interest: geodataframe avec la geometrie d interet
     :param buffer_size_for_protected_buildings: taille du buffer autour des batiments proteges
-    :param crs: projection
+    :param crs: projection de la gdf renvoyee
+    :param crs_for_buffers: crs utilise pour le calcul des buffers (en metres)
     :return: gdf des zones avec des batiments proteges
     """
     protected_buildings = gpd.read_file(
         bd_protected_buildings_path, mask=geom_of_interest
-    ).to_crs(crs)
+    ).to_crs(crs_for_buffers)
 
     areas_with_protected_buildings = protected_buildings.buffer(
         buffer_size_for_protected_buildings,
         cap_style="round"
     )
 
-    return areas_with_protected_buildings
+    return areas_with_protected_buildings.to_crs(crs)
