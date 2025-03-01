a63db21d2ba2e5fb6be8806c23da32cc17d6e6e4
Luc J <kelu124@gmail.com>
2025-03-01 15:30:28 +0100

    Adding new WNS url to lib

--- algorithme/potentiel_solaire/features/roof_attributes.py
+++ algorithme/potentiel_solaire/features/roof_attributes.py
@@ -44,7 +44,9 @@ def recuperation_flux_wms(boite: list[int], layer: str,
     - Uses WMS version 1.3.0 for the request
     """
 
-    url = "https://data.geopf.fr/wms-r/wms?SERVICE=WMS"
+    url = "https://data.geopf.fr/annexes/ressources/wms-r/essentiels.xml"
+    # Originally "https://data.geopf.fr/wms-r/wms?SERVICE=WMS" 
+    # but failed with a 'content-type' error.
     wms = WebMapService(url, version='1.3.0')
 
     img_mns = wms.getmap(layers=[layer],
