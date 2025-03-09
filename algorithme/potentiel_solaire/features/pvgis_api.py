496cb217d6c3ce45515160ebda77d004c830614b
AmaurySalles <amaury.salles@gmail.com>
2025-03-10 00:18:06 +0100

    calculate solar potential pipeline v0

--- algorithme/potentiel_solaire/features/pvgis_api.py
+++ algorithme/potentiel_solaire/features/pvgis_api.py
@@ -3,27 +3,33 @@ File containing the pipeline methodo to call the PVGIS API and add relevant sola
 assumptions.
 https://joint-research-centre.ec.europa.eu/photovoltaic-geographical-information-system-pvgis_en
 """
-
-import random
 from time import sleep
 import geopandas as gpd
 import json
 import requests
 
-from potentiel_solaire.constants import CRS_FOR_BUFFERS, USEABLE_SURFACE
+from potentiel_solaire.logger import get_logger
+
+
+logger = get_logger()
 
 
 PVGIS_BASE_URL = "https://re.jrc.ec.europa.eu/api/v5_2/PVcalc?&"
 
-
 DEFAULT_QUERY_PARAMS = {
-    'loss': 20, # The system's losses in percentage. Recommend between 15 - 30 %
+    'loss': 14, # The system's losses in percentage. Recommend between 15 - 30 %
     'fixed': 1,  # Fixed versus solar tracking system. Fixed in case of solar rooftop.
     'mountingplace': 'building', # Param should impacts losses. We may be double counting.
-    
+    'optimalangles': 1, # Letting the engine optimise the ti
 }
 
-BUILDING_QUERY_PARAMS = [
+V0_OPTIMISED_PVGIS_QUERY_PARAMS = [
+    'lat', 
+    'lon',
+    'peakpower',
+]
+
+V0_BASIC_QUERY_PARAMS = [
     'lat', 
     'lon',
     'angle',
@@ -31,79 +37,39 @@ BUILDING_QUERY_PARAMS = [
     'peakpower'
 ]
 
-def prepare_df_for_pvgis_call(df: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
+def build_query_params_url(params: gpd.GeoSeries) -> str:
     """
-    Method used to extract, transform and add relevant fields required for the 
-    PV GIS API call. Notably:
-    - Latitude & longitude of building center in deg decimal
-    - Roof tilt angle in deg
-    - Roof orientation (aspect) in deg
-    - Useable roof area in m2
-    - Installed capacity (peakpower) in kW
+    Helper function building the PV GIS API query url with all requested params.
+    Adds a check on the only required parameters.
     """
-    df['lon'] = df.centroid.map(lambda p: p.x)
-    df['lat'] = df.centroid.map(lambda p: p.y)
-    df = df.to_crs(epsg=CRS_FOR_BUFFERS)
+    if (params['peakpower'] <= 0) or params[['lat', 'lon']].isna().any():
+        return None
     
-    # TODO: Currently makeshift values
-    df['angle']  = df['lat'].map(lambda x: random.randint(0, 90))
-    df['aspect'] = df['lat'].map(lambda x: random.randint(0, 360)) # azimuth
-    df = _apply_system_assumptions(df)
-    df = _apply_constraints(df)
-    return df
-
-
-def _apply_system_assumptions(df: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
-    """
-    Method aggregating all PV estimation assumptions prior to calculation / call to PVGIS API.
-    Notably:
-    - Reduce available surface to account for other installations, space between panels, etc.
-    - Transform useable surface into installed capacity (peakpower), with assumption that a 
-    typical PV panel produces 225W/m2, as per some modules here https://www.csisolar.com/module/.
-    """
-    df['useable_surface'] = df.area.map(lambda x: x * USEABLE_SURFACE)
-    df['peakpower'] = df['useable_surface'].map(lambda x: x * 225 / 1_000) # in kW
-    return df
-    
-    
-def _apply_constraints(df: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
-    """
-    Method regrouping all methodological constraints, notably:
-    - Exclude roofs badly exposed (from North-West (180) to North-East (360))
-    - Exclude roofs with too big slope
-    - Exclude roofs that are too small (less than 10m2)
-    """
-    df = df[df['aspect'] < 180]
-    df = df[df['angle'] < 60]
-    df = df[df['useable_surface'] > 10]
-    return df
-
-
-
-def call_pvgis_api(df: gpd.GeoDataFrame) -> dict:
-    """
-    Method used to build api url and calls the PVGIS API.
-    TODO: Limiting requests to 3 during PoC. To remove when wanting to run through entire pipeline.
-    NOTE: Added sleep timer to ensure that we do not exceed the 30 calls / second rate limit.
-    """
-    counter = 0
-    for idx, building in df.iterrows():
-        url = _build_query_params_url(building[BUILDING_QUERY_PARAMS])
-        df.loc[idx, 'url'] = url
-        if counter < 3 :
-            if response := requests.get(url):
-                row_json = json.loads(response.text)
-                df.loc[idx, 'annual_prod (kWh/y)'] = row_json['outputs']['totals']['fixed']['E_y']
-                counter += 1
-                sleep(0.03)
-    return df
-
-
-def _build_query_params_url(params: gpd.GeoSeries) -> str:
-    """
-    Helper function building the PV GIS API query url with all requested params
-    """
     url= PVGIS_BASE_URL + "&".join([f'{key}={value}' for key, value 
                                     in DEFAULT_QUERY_PARAMS.items()]) + '&'
     url+= "&".join([f'{idx}={params[idx]}' for idx in params.index])
     return url + '&outputformat=json'
+
+
+def call_pvgis_api(query: str | None) -> float | None:
+    """
+    Method used to build api url and calls the PVGIS API.
+    
+    Returns the annual energy production (MWh/yr)
+    
+    NOTE: Added sleep timer to ensure that we do not exceed the 30 calls / second rate limit.
+    TODO: There are many more output parameters available.
+    """
+    if not query:
+        return None
+    
+    try:
+        if response := requests.get(query):
+            row_json = json.loads(response.text)
+            sleep(0.04)
+            return row_json['outputs']['totals']['fixed']['E_y'] / 1_000
+        logger.error(f'Failed to query API. Response: {response}')
+        return None
+    except Exception as e:
+        logger.error(e)
+        return None
--- algorithme/potentiel_solaire/features/solar_potential.py
+++ algorithme/potentiel_solaire/features/solar_potential.py
@@ -1,13 +1,16 @@
 import geopandas as gpd
 
+from potentiel_solaire.features.pvgis_api import V0_BASIC_QUERY_PARAMS, V0_OPTIMISED_PVGIS_QUERY_PARAMS, build_query_params_url, call_pvgis_api
+from potentiel_solaire.logger import get_logger
 from potentiel_solaire.features.solar_exposition import calculate_solar_exposition_building
-from potentiel_solaire.features.roof_attributes import calculate_surface_utile
 from potentiel_solaire.features.protected_tag import link_protected_buildings
 from potentiel_solaire.constants import (
     RENDEMENT_PANNEAU_PV,
     BUFFER_SIZE_FOR_SOLAR_EXPOSITION,
     CRS_FOR_BUFFERS
 )
+
+logger = get_logger()
 
 
 def calculate_solar_potential(
@@ -26,45 +29,79 @@ def calculate_solar_potential(
     :param buffer_for_solar_exposition: distance autour du batiment a prendre en compte pour le rayonnement solaire
     :param rendement_panneau_pv: ration du rendement d un panneau solaire
     :param crs_for_buffers: crs utilise pour le calcul des buffers (en metres)
-    :return: le geodataframe des batiments scolaires avec les features de potentiel solaire
+    :return: le geodataframe des batiments scolaires avec les features de potentiel solaire et le
+    productible annuel estimé par l'API PVGIS.
     """
+    # Extrait latitude & longitude de la geometry
+    schools_buildings['lon'] = schools_buildings.centroid.map(lambda p: p.x)
+    schools_buildings['lat'] = schools_buildings.centroid.map(lambda p: p.y)
+    
+    
+    # Ajout du tag batiments proteges ou en zone protegee
+    schools_buildings["protection"] = schools_buildings["geometry"].map(
+        lambda geometry: link_protected_buildings(
+            building=geometry,
+            areas_with_protected_buildings=areas_with_protected_buildings
+        )
+    )
 
     # Calcul de la surface totale au sol & zone de 2km autour des batiment
     crs_init = schools_buildings.crs
     schools_buildings = schools_buildings.to_crs(epsg=crs_for_buffers)
     schools_buildings["surface_totale_au_sol"] = schools_buildings.area
-    schools_buildings["zone_pour_rayonnement_solaire"] = schools_buildings.buffer(
-        distance=buffer_for_solar_exposition
-    ).to_crs(crs_init)
+    schools_buildings["zone_pour_rayonnement_solaire"] = (
+        schools_buildings.buffer(distance=buffer_for_solar_exposition).to_crs(crs_init)
+    )
     schools_buildings = schools_buildings.to_crs(crs_init)
+    
+    
 
-    # Calcul de la surface utile
-    schools_buildings["surface_utile"] = schools_buildings.apply(
-       lambda building: calculate_surface_utile(
-           surface_totale_au_sol=building["surface_totale_au_sol"]
-       ), axis=1
+    # Calcul de la surface de toit utile
+    schools_buildings["surface_utile"] = schools_buildings["surface_totale_au_sol"].map(
+       lambda surface_totale: calculate_surface_utile(surface_totale)
     )
-
-    # Calcul de l exposition solaire
-    schools_buildings["rayonnement_solaire"] = schools_buildings.apply(
-       lambda building: calculate_solar_exposition_building(
-           zone_for_solar_exposition=building["zone_pour_rayonnement_solaire"],
-           bd_irradiation_path=bd_irradiation_path
-       ), axis=1
+    
+    # Calcul de la capacité installé
+    schools_buildings["peakpower"] = schools_buildings['surface_utile'].map(
+        lambda surface_utile: calculate_installed_capacity(surface_utile)
     )
+    
 
-    # Calcul potentiel solaire
-    schools_buildings["potentiel_solaire"] = \
-        schools_buildings["rayonnement_solaire"] * \
-        schools_buildings["surface_utile"] * \
-        rendement_panneau_pv
-
-    # Ajout du tag batiments proteges ou en zone protegee
-    schools_buildings["protection"] = schools_buildings.apply(
-        lambda building: link_protected_buildings(
-            building=building["geometry"],
-            areas_with_protected_buildings=areas_with_protected_buildings
-        ), axis=1
+    # Construire la requête pour PVGIS
+    schools_buildings['url'] = schools_buildings[V0_OPTIMISED_PVGIS_QUERY_PARAMS].apply(
+        lambda query_params: build_query_params_url(query_params), axis=1
+    )
+    
+    # # Requête API PVGIS
+    schools_buildings['productible'] = schools_buildings[['url', 'peakpower']].apply(
+        lambda x: call_pvgis_api(x['url']) if x['peakpower'] > 0 else 0, axis=1
     )
 
     return schools_buildings
+
+
+def calculate_surface_utile(surface_totale_au_sol: float):
+    """Calcule la surface utile pour le PV.
+
+    Pour le moment il s agit d'un simple ratio.
+    @TODO Remplacer par une formule plus fine
+
+    :param surface_totale_au_sol: surface totale au sol du batiment
+    :return: la surface utile pour installation de panneaux PV
+    """
+    if surface_totale_au_sol <= 100:
+        return 0
+
+    if 100 < surface_totale_au_sol < 500:
+        ratio = 0.4 * surface_totale_au_sol / 5000 + 0.2
+        return ratio * surface_totale_au_sol
+
+    return 0.6 * surface_totale_au_sol
+
+
+def calculate_installed_capacity(rooftop_surface: float) -> float:
+    """
+    Transform useable surface into installed capacity (peakpower), with assumption that a 
+    typical PV panel produces 225W/m2, as per some modules here https://www.csisolar.com/module/.
+    """
+    return rooftop_surface * 225 / 1_000 # in kW
