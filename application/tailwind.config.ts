1797159f62cca3da80f01dfe75dc057d60725104
Oceane BOUNAN <bounan.oceane@orange.fr>
2025-03-13 22:49:12 +0400

    Prepare customisation Tailwind

--- application/tailwind.config.ts
+++ application/tailwind.config.ts
@@ -1,7 +1,27 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: "class",
   content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+        accent: "var(--color-accent)",
+      },
+      fontFamily: {
+        sans: ["var(--font-sans)", "sans-serif"],
+        serif: ["var(--font-serif)", "serif"],
+      },
+      spacing: {
+        // sm: "rem",
+        // md: "rem",
+        // lg: "rem",
+        // xl: "rem",
+      },
+      borderRadius: {},
+    },
+  },
   plugins: [],
 } satisfies Config;
