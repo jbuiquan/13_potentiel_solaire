7567617babc42d17f83d418c2d94749d926057e7
machbry <matthieu.chambery@gmail.com>
2025-03-16 13:12:30 +0100

    ajout possibilite d exporter les donnees de duckdb dans un format gpkg

--- algorithme/potentiel_solaire/geojson_files_creator.py
+++ algorithme/potentiel_solaire/geojson_files_creator.py
@@ -1,24 +1,31 @@
-import warnings
 import pandas as pd
 import geopandas as gpd
 import shapely.geometry
 import json
-from potentiel_solaire.logger import get_logger
 
-warnings.filterwarnings("ignore", message="'crs' was not provided", category=UserWarning, module="pyogrio")
+from potentiel_solaire.constants import DEFAULT_CRS
+from potentiel_solaire.logger import get_logger
 
 logger = get_logger()
 
-def export_to_geojson(
+
+def export_to_geofile(
     stats: pd.DataFrame,
-    output_filepath: str) -> bool:
+    output_filepath: str,
+    driver: str = 'GeoJSON',
+    layer: str = None,
+    crs: int = DEFAULT_CRS
+) -> bool:
     """
-    Exporte un DataFrame Pandas contenant des géométries au format GeoJSON dans un fichier GeoJSON.
+    Exporte un DataFrame Pandas contenant des géométries dans un fichier au format souhaite.
 
     Args:
         stats (pd.DataFrame): Le DataFrame contenant les statistiques, avec une colonne nommée 'geometry' qui contient des géométries 
                                au format GeoJSON (sous forme de chaînes JSON).
-        output_filepath (str): Le chemin complet du fichier de sortie GeoJSON.
+        output_filepath (str): Le chemin complet du fichier de sortie
+        driver (str): Le format du fichier de sortie (.geojson, .gpkg, ...)
+        layer (str): Le nom du layer si le format de sortie est GPKG
+        crs (int): Le crs du fichier de sortie
 
     Returns:
         bool: Retourne `True` si l'exportation a réussi, sinon lève une exception.
@@ -34,7 +41,7 @@ def export_to_geojson(
         >>>     'geometry': ['{"type":"Polygon","coordinates":[[[2.319887543, 48.8588443]]]}', 
         >>>                  '{"type":"Polygon","coordinates":[[[2.319888, 48.858845]]]}']
         >>> })
-        >>> export_to_geojson(df, "output.geojson")
+        >>> export_to_geofile(df, "output.geojson")
         True
 
     Notes:
@@ -65,11 +72,16 @@ def export_to_geojson(
         if df['geometry'].empty:
             raise ValueError("Aucune géométrie valide après conversion.")
 
-        gdf = gpd.GeoDataFrame(df, geometry='geometry')
-        gdf.to_file(output_filepath, driver='GeoJSON')
+        gdf = gpd.GeoDataFrame(df, geometry='geometry', crs=crs)
+
+        if driver == 'GPKG':
+            gdf.to_file(output_filepath, layer=layer, driver=driver)
+        else:
+            gdf.to_file(output_filepath, driver=driver)
+
         logger.info("Fichier '%s' créé avec succès ✅", output_filepath)
 
         return True
     except Exception as e:
-        logger.error(f"Erreur lors de l'exportation vers GeoJSON: {e}")
-        raise Exception(f"Erreur lors de l'exportation vers GeoJSON: {e}") from e
+        logger.error(f"Erreur lors de l'exportation vers {driver}: {e}")
+        raise Exception(f"Erreur lors de l'exportation vers {driver}: {e}") from e
