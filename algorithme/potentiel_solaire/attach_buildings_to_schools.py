db8e80d3dd313d1fd34c3badd451af89ba7d5033
machbry <matthieu.chambery@gmail.com>
2025-03-16 21:02:34 +0100

    refacto pour une meilleure comprehension du code

--- algorithme/potentiel_solaire/attach_buildings_to_schools.py
+++ algorithme/potentiel_solaire/attach_buildings_to_schools.py
@@ -1,30 +1,38 @@
-import pandas as pd
 import geopandas as gpd
-from thefuzz.fuzz import token_sort_ratio
 
 from potentiel_solaire.logger import get_logger
 
 logger = get_logger()
 
 
-def attach_buildings_schools_id_men(
+def attach_educational_zones_to_schools(
     schools_establishments: gpd.GeoDataFrame,
-        educational_zones: gpd.GeoDataFrame,
-    ) -> gpd.GeoDataFrame:
-    """@TODO DOCSTRING"""
+    educational_zones: gpd.GeoDataFrame,
+) -> gpd.GeoDataFrame:
+    """Determine les zones d education de chaque etablissement scolaire
+
+    :param schools_establishments: annuraires des etablissements scolaires
+    :param educational_zones: zones d educations (BD TOPO)
+    :return: geodataframe des zones scolaires
+    """
     # Preparation 'educational_zones'
     educational_zones = educational_zones.dropna(subset=["identifiants_sources"])
-    educational_zones["identifiants_sources"] = educational_zones["identifiants_sources"].apply(lambda x: [y.strip() for y in str(x).replace("MEN:","").split("/")])
+    educational_zones["identifiants_sources"] = educational_zones["identifiants_sources"].apply(
+        lambda x: [y.strip() for y in str(x).replace("MEN:", "").split("/")]
+    )
     educational_zones = educational_zones.explode('identifiants_sources')
 
-    # Recollement
-    educational_zones_attached_to_schools = pd.merge(
-        educational_zones,
-        schools_establishments[["identifiant_de_l_etablissement","nom_etablissement"]],
-        left_on='identifiants_sources', right_on='identifiant_de_l_etablissement',
-        how="inner")
+    # Rattachement
+    educational_zones_attached_to_schools = educational_zones.merge(
+        schools_establishments[["identifiant_de_l_etablissement", "nom_etablissement"]],
+        left_on='identifiants_sources',
+        right_on='identifiant_de_l_etablissement',
+        how="inner"
+    )
     # Cleanup
-    educational_zones_attached_to_schools = educational_zones_attached_to_schools.dropna(subset=["cleabs","identifiants_sources"])
+    educational_zones_attached_to_schools = educational_zones_attached_to_schools.dropna(
+        subset=["cleabs", "identifiants_sources"]
+    )
     return educational_zones_attached_to_schools
 
 
@@ -41,7 +49,10 @@ def attach_buildings_to_schools(
     :return: geodataframe des batiments scolaires
     """
 
-    educational_zones_attached_to_schools = attach_buildings_schools_id_men(schools_establishments, educational_zones)
+    educational_zones_attached_to_schools = attach_educational_zones_to_schools(
+        schools_establishments=schools_establishments,
+        educational_zones=educational_zones
+    )
 
     # On ne garde que les batiments qui sont dans des zones affectees a une ecole
     buildings_for_schools = gpd.sjoin(
