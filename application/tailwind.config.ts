982e5279542fb85737b0336779aaddc9a373c985
Oce-ane <163306560+Oce-ane@users.noreply.github.com>
2025-03-17 18:13:01 +0400

    [FRONT] Prepare customisation Tailwind (#105)
    * Prepare customisation Tailwind
    
    * import corrigé
    
    * correction dark & exemple utilisation config

--- application/tailwind.config.ts
+++ application/tailwind.config.ts
@@ -1,7 +1,28 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: "selector",
   content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+        accent: "var(--color-accent)",
+      },
+      fontFamily: {
+        sans: ["var(--font-sans)", "sans-serif"],
+        serif: ["var(--font-serif)", "serif"],
+      },
+      spacing: {
+        xs: "0.5rem",
+        sm: "1rem",
+        md: "1.5rem",
+        lg: "2rem",
+        xl: "3rem",
+      },
+      borderRadius: {},
+    },
+  },
   plugins: [],
 } satisfies Config;
