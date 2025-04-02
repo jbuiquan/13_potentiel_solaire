4ab2d25687d453e5edfb724ee4a618e01df43007
Kévin Tessier <kevin-tessier@protonmail.com>
2025-04-02 11:29:08 +0200

    Add : config var tailwind.config

--- application/tailwind.config.ts
+++ application/tailwind.config.ts
@@ -6,14 +6,36 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: "var(--color-primary)",
-        secondary: "var(--color-secondary)",
-        accent: "var(--color-accent)",
+        'sol-ok': "#FAEA5E",
+        'sol-top': "#E19803",
+        'sol-ko': "#FFFBD6",
+		'BG-darkmode':"#221C3E",
+		'light-green': "#E7FFD3",
+		'gray': "#5D5B65",
+		'dark-green': "#355917",
+		'gray-light-bg': "#FAFAFA",
+		'select': "#A29DB9",
       },
       fontFamily: {
-        sans: ["var(--font-sans)", "sans-serif"],
-        serif: ["var(--font-serif)", "serif"],
+        verdana: ["verdana", "open-sans"],
       },
+	  fontSize:{
+		sm: '0.875rem',
+		base: '1rem',
+		lg:'2rem',
+		xl:'2.5rem',
+	  },
+	  lineHeight:{
+		normal:'100%',
+		md:'1.313rem',
+		lg:'1.375rem',
+		xl:'1.75rem',
+		'2xl':'2.813rem',
+	  },
+	  letterSpacing: {
+		sm: '-3%',
+		lg: '50%',
+	  },
       spacing: {
         xs: "0.5rem",
         sm: "1rem",
@@ -22,6 +44,9 @@ export default {
         xl: "3rem",
       },
       borderRadius: {},
+	  boxShadow: {
+		'base': '0px 4px 4px 0px rgba(0, 0, 0, 0.25)',
+	  }
     },
   },
   plugins: [],
