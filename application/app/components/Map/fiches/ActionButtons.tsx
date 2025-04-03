9cabffe6e412c5b65471d21d5660257036bd1021
Oceane BOUNAN <bounan.oceane@orange.fr>
2025-04-03 14:59:13 +0400

    feat: create ActionButtons component

---
+++ application/app/components/Map/fiches/ActionButtons.tsx
@@ -0,0 +1,11 @@
+import {Share2, Download} from "lucide-react";
+
+const ActionButtons = () => {
+  return (
+    <div className="flex gap-4">
+        <Share2 />
+        <Download />
+    </div>
+  );
+};
+export default ActionButtons;
