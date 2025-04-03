5bbe73c789dbd55b03bd16d88cc933e16358c8f5
Oceane BOUNAN <bounan.oceane@orange.fr>
2025-04-03 15:02:38 +0400

    feat: create GraphiqueCard component

---
+++ application/app/components/Map/fiches/GraphiqueCard.tsx
@@ -0,0 +1,8 @@
+const GraphiqueCard = () => {
+  return (
+    <div className="h-40 bg-gray-300 flex items-center justify-center my-4">
+      Graphique ici
+    </div>
+  );
+};
+export default GraphiqueCard;
