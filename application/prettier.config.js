ca271f315a360dcfc3dd0352619d3dd1022ceed0
Oceane BOUNAN <bounan.oceane@orange.fr>
2025-04-06 18:30:17 +0400

    chore: format files with prettier

--- application/prettier.config.js
+++ application/prettier.config.js
@@ -3,17 +3,17 @@
  * @type {import("prettier").Config}
  */
 const config = {
-    'semi': true,
-    'tabWidth': 4,
-    'printWidth': 100,
-    'singleQuote': true,
-    'trailingComma': 'all',
-    'jsxSingleQuote': true,
-    'bracketSpacing': true,
-    'importOrder': ['^react', '^next', '<THIRD_PARTY_MODULES>', '^@components/(.*)$', '^[./]'],
-    'importOrderSeparation': true,
-    'importOrderSortSpecifiers': true,
-    'plugins': ['@trivago/prettier-plugin-sort-imports', 'prettier-plugin-tailwindcss']
-  };
+	semi: true,
+	tabWidth: 4,
+	printWidth: 100,
+	singleQuote: true,
+	trailingComma: 'all',
+	jsxSingleQuote: true,
+	bracketSpacing: true,
+	importOrder: ['^react', '^next', '<THIRD_PARTY_MODULES>', '^@components/(.*)$', '^[./]'],
+	importOrderSeparation: true,
+	importOrderSortSpecifiers: true,
+	plugins: ['@trivago/prettier-plugin-sort-imports', 'prettier-plugin-tailwindcss'],
+};
 
-  module.exports = config;
+module.exports = config;
