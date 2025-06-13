1801adf2122b663dce6c78f245a01e458934a7a1
Oceane BOUNAN <bounan.oceane@orange.fr>
2025-06-13 23:23:41 +0200

    chore: correct link for top 3

--- application/app/components/fiches/shared/TopCard.tsx
+++ application/app/components/fiches/shared/TopCard.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 
+import { TopEtablissement } from '@/app/models/etablissements';
 import { Sun } from 'lucide-react';
 
 const UNKNOWN_TEXTS = {
@@ -8,14 +9,8 @@ const UNKNOWN_TEXTS = {
 
 const medals = ['🥇', '🥈', '🥉'];
 
-type Etablissement = {
-	id: string;
-	libelle: string;
-	potentiel_solaire: number;
-};
-
 type Props = {
-	topEtablissements: Etablissement[] | null;
+	topEtablissements: TopEtablissement[] | null;
 };
 
 const TopCard = ({ topEtablissements }: Props) => {
@@ -34,8 +29,7 @@ const TopCard = ({ topEtablissements }: Props) => {
 					<li key={etab.id}>
 						{medals[index]}{' '}
 						<Link
-							// the href might need to be corrected
-							href={`/etablissement/${etab.id}`}
+							href={`/?codeEtablissement=${etab.id}&isFicheOpen=true`}
 							className='underline decoration-dotted decoration-2 underline-offset-4 transition hover:text-primary'
 						>
 							{etab.libelle}
