57926b642a719336c39cc8de5de955059e481178
Oceane BOUNAN <bounan.oceane@orange.fr>
2025-06-16 21:14:48 +0200

    feat: add notre methodologie page

---
+++ application/app/notre-methodologie/page.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import StaticPage from '../components/StaticPage';
+import notreMethodologieContent from '../components/content/notre-methodologie';
+
+export default function NotreMethodologiePage() {
+	return <StaticPage {...notreMethodologieContent} />;
+}
