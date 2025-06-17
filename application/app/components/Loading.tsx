913caaa2aeaafb1978bb554a26e47ffc91ed4f0e
Oceane BOUNAN <bounan.oceane@orange.fr>
2025-06-17 20:23:33 +0200

    feat: eaa compliance for loader component

--- application/app/components/Loading.tsx
+++ application/app/components/Loading.tsx
@@ -2,8 +2,14 @@ import { Loader } from 'lucide-react';
 
 export default function Loading() {
 	return (
-		<div className='flex h-[100%] w-[100%] items-center justify-center text-center'>
+		<div
+			className='flex h-full w-full items-center justify-center text-center'
+			role='status'
+			aria-live='polite'
+			aria-label='Chargement en cours'
+		>
 			<Loader className='animate-spin text-primary' />
+			<span className='sr-only'>Chargement en cours</span>
 		</div>
 	);
 }
