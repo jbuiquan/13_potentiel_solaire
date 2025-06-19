114dcc16334cce24445c191f2591069b0811b9a7
Oceane BOUNAN <bounan.oceane@orange.fr>
2025-06-19 21:31:07 +0200

    feat: eaa compliance for etablissementcard component

--- application/app/components/fiches/ficheEtablissement/EtablissementCard.tsx
+++ application/app/components/fiches/ficheEtablissement/EtablissementCard.tsx
@@ -2,28 +2,35 @@ import { Etablissement } from '@/app/models/etablissements';
 
 type EtablissementCardProps = Pick<
 	Etablissement,
-	'nom_etablissement' | 'adresse_1' | 'adresse_2' | 'adresse_3'
+	'nom_etablissement' | 'adresse_1' | 'adresse_2' | 'adresse_3' | 'code_postal' | 'nom_commune'
 >;
 
 const UNKNOWN_TEXTS = {
-	name: 'Nom inconnu',
+	name: "Nom de l'établissement inconnu",
 	adresse: 'Adresse inconnue',
 };
 
-//TODO: if adresse_3 is null => use code_postal + nom_commune (à confirmer au niveau data)
 const EtablissementCard = ({
 	nom_etablissement,
 	adresse_1,
 	adresse_2,
 	adresse_3,
+	code_postal,
+	nom_commune,
 }: EtablissementCardProps) => {
 	return (
-		<div className='text-blue'>
+		<article className='text-blue'>
 			<h1 className='text-2xl font-bold'>{nom_etablissement || UNKNOWN_TEXTS.name}</h1>
-			<p>{adresse_1 || UNKNOWN_TEXTS.adresse}</p>
-			{adresse_2 && <p>{adresse_2}</p>}
-			{adresse_3 && <p>{adresse_3}</p>}
-		</div>
+			<address className='not-italic'>
+				<p>{adresse_1 || UNKNOWN_TEXTS.adresse}</p>
+				{adresse_2 && <p>{adresse_2}</p>}
+				{adresse_3 ? (
+					<p>{adresse_3}</p>
+				) : (
+					code_postal && nom_commune && <p>{`${code_postal} ${nom_commune}`}</p>
+				)}
+			</address>
+		</article>
 	);
 };
 
