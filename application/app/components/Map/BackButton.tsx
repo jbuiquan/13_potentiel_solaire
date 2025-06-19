a483cccab90618d1f61dfb26451a2c2512d05df0
Oceane BOUNAN <bounan.oceane@orange.fr>
2025-06-19 21:28:35 +0200

    feat: eaa compliance for backbutton component

--- application/app/components/Map/BackButton.tsx
+++ application/app/components/Map/BackButton.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@/components/ui/button';
+import { ChevronLeft } from 'lucide-react';
 
 type BackButtonProps = {
 	onBack: () => void;
@@ -6,7 +7,8 @@ type BackButtonProps = {
 
 export default function BackButton({ onBack }: BackButtonProps) {
 	return (
-		<Button onClick={onBack} className='absolute right-4 top-4'>
+		<Button onClick={onBack} className='absolute right-4 top-4' aria-label='Retour'>
+			<ChevronLeft className='mr-2 h-4 w-4' />
 			Retour
 		</Button>
 	);
