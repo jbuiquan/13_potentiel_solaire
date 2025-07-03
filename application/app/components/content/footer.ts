1db8a287e4854f0c4ca16246c8d8db7b604520c1
Oceane BOUNAN <bounan.oceane@orange.fr>
2025-07-04 00:14:18 +0200

    chore: kebab-case convention for content files

---
+++ application/app/components/content/footer.ts
@@ -0,0 +1,41 @@
+export const footerLinks = [
+	{ title: 'Mentions légales', href: '/mention-legales' },
+	{
+		title: 'Jeu de données ouvert',
+		href: 'https://github.com/dataforgoodfr/13_potentiel_solaire',
+	},
+	{ title: 'Accessibilité du site', href: '/accessibilite-du-site' },
+];
+
+export const partners = [
+	{
+		name: 'Greenpeace',
+		logo: '/images/greenpeace.svg',
+		address: '13 rue d’Enghien',
+		city: '75010 Paris',
+		phone: '01 80 96 96 96',
+	},
+	{
+		name: 'Data For Good',
+		logo: '/images/DataForGood.svg',
+		socials: [
+			{ name: 'Github', href: 'https://github.com/dataforgoodfr' },
+			{ name: 'Hugging Face', href: 'https://huggingface.co/DataForGood' },
+			{ name: 'YouTube', href: 'https://www.youtube.com/channel/UCA_utdbmVhAOFujulWlaaCQ' },
+			{ name: 'Twitch', href: 'https://www.twitch.tv/dataforgood' },
+			{ name: 'Meetup', href: 'https://www.meetup.com/data-for-good-fr/' },
+		],
+	},
+];
+
+export const footerText = {
+	copyright: 'Tous droits réservés — © 2025 Greenpeace & Data for Good',
+};
+
+export const footerDescription = {
+	title: 'Notre objectif',
+	text: `L’outil Établissement Solaire permet d’évaluer le potentiel des toitures des établissements
+scolaires français pour accueillir des panneaux solaires photovoltaïques. Interactif, avec des données accessibles et libres,
+il permet d’aider les habitant·es et les collectivités locales à connaître le potentiel de leur territoire et à agir pour
+développer les énergies renouvelables près de chez eux.`,
+};
