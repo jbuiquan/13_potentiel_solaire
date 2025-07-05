b7202266e59be131adbe4fee3772d6b2a46b6da0
machbry <matthieu.chambery@gmail.com>
2025-07-05 14:38:54 +0200

    add separate command for attach_buildings_to_schools pipeline

---
+++ algorithme/potentiel_solaire/etl/transform.py
@@ -0,0 +1,63 @@
+from potentiel_solaire.classes.results import DepartementResults
+from potentiel_solaire.classes.source import load_sources
+from potentiel_solaire.constants import DEFAULT_CRS
+from potentiel_solaire.features.attach_buildings_to_schools import attach_buildings_to_schools
+from potentiel_solaire.features.protected_tag import link_protected_buildings
+from potentiel_solaire.sources.bd_topo import find_gpkg_file_bd_topo, get_topo_buildings_of_interest, get_topo_zones_of_interest
+from potentiel_solaire.sources.protected_buildings import get_areas_with_protected_buildings
+from potentiel_solaire.logger import get_logger
+
+logger = get_logger()
+
+
+def attach_buildings_to_schools_pipeline(codes_departement: list[str]):
+    """Transformation des donnees pour plusieurs departements.
+
+    Args:
+        codes_departement (list[str]): Codes des departements pour lesquels les donnees doivent etre transformees.
+    """
+    for code_departement in codes_departement:
+        # Lecture des donnees extraites pour le departement
+        results = DepartementResults(code_departement=code_departement)
+        bd_topo_path = find_gpkg_file_bd_topo(code_departement=code_departement)
+        geom_of_interest = results.load_gdf(layer="geom_of_interest")
+        schools_establishments = results.load_gdf(layer="schools_establishments")
+
+        # Determination des zone scolaires associees
+        educational_zones = get_topo_zones_of_interest(
+            bd_topo_path=bd_topo_path,
+            geom_of_interest=geom_of_interest,
+            categories=["Science et enseignement"],
+            natures=['Collège', 'Lycée', 'Enseignement primaire'],
+            crs=DEFAULT_CRS
+        )
+        nb_educational_zones = educational_zones.shape[0]
+        logger.info("Nb de zones d'éducations: %s", nb_educational_zones)
+
+        # Determination des batiments sur la geometrie d'interet
+        buildings = get_topo_buildings_of_interest(
+            bd_topo_path=bd_topo_path,
+            geom_of_interest=geom_of_interest,
+            crs=DEFAULT_CRS
+        )
+        nb_buildings = buildings.shape[0]
+        logger.info("Nb de batiments: %s", nb_buildings)
+
+        # Attachement des batiments et zones scolaires aux etablissements scolaires
+        schools_buildings = attach_buildings_to_schools(
+            schools_establishments=schools_establishments,
+            educational_zones=educational_zones,
+            buildings=buildings
+        )
+        nb_schools_buildings = schools_buildings.shape[0]
+        logger.info("Nb de batiments scolaires: %s", nb_schools_buildings)
+
+        # Sauvegarde des resultats
+        results.save_gdf(
+            gdf=educational_zones,
+            layer="educational_zones"
+        )
+        results.save_gdf(
+            gdf=schools_buildings,
+            layer="schools_buildings"
+        )
